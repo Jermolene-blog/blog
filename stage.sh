61486f15cef2f5e15b6989a67b022a38f2d014f2
Jermolene <jeremy@jermolene.com>
2015-09-28 19:00:19 +0100

    Add the CNAME record

--- stage.sh
+++ stage.sh
@@ -5,3 +5,7 @@ cp -R main-wiki/output/* ../jermolene-blog.github.io/
 mkdir -p ../jermolene-blog.github.io/talkytalky/
 
 cp -R talkytalky-wiki/output/* ../jermolene-blog.github.io/talkytalky/
+
+# Make the CNAME file that GitHub Pages requires
+
+echo "jermolene.com" > ../jermolene-blog.github.io/CNAME
