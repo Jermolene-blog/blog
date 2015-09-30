80948af0052650d015ae0de78dcfb74f71499292
Jermolene <jeremy@jermolene.com>
2015-09-30 22:12:19 +0100

    Talkytalky: fix Cecily
    By rearranging the handling of static assets

--- bld.sh
+++ bld.sh
@@ -2,12 +2,9 @@
 
 # build all output
 
-rm main-wiki/output/static/*
-rm talkytalky-wiki/output/static/*
+rm -Rf main-wiki/output/*
+rm -Rf talkytalky-wiki/output/*
 
 tiddlywiki main-wiki --verbose --build
-
-rm main-wiki/output/cecily/index.html
-mv main-wiki/output/cecily/cecily.html main-wiki/output/cecily/index.html
 
 tiddlywiki talkytalky-wiki --verbose --build index
--- stage.sh
+++ stage.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+rm -Rf ../jermolene-blog.github.io/*
+
 cp -R main-wiki/output/* ../jermolene-blog.github.io/
 
 mkdir -p ../jermolene-blog.github.io/talkytalky/
 
 cp -R talkytalky-wiki/output/* ../jermolene-blog.github.io/talkytalky/
 
-# Make the CNAME file that GitHub Pages requires
-
-echo "jermolene.com" > ../jermolene-blog.github.io/CNAME
+cp -R static-assets/* ../jermolene-blog.github.io/
